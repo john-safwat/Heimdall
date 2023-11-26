8bc1a03d48d84fe9be35c5b71becd665da15cc88
John Safwat <john362sss@gmail.com>
2023-11-27 01:46:42 +0200

    add bottom navigation bar

--- lib/Presentation/UI/Home/HomeViewModel.dart
+++ lib/Presentation/UI/Home/HomeViewModel.dart
@@ -3,5 +3,11 @@ import 'package:heimdall/Presentation/UI/Home/HomeNavigator.dart';
 
 class HomeViewModel extends BaseViewModel <HomeNavigator> {
 
+  int selectedIndex = 0;
+
+  changeIndex(int index){
+    selectedIndex = index;
+    notifyListeners();
+  }
 
 }
