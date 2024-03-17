1e39d5bdb6c5f66679218ebe770927f92d732662
John Safwat <john362sss@gmail.com>
2024-03-17 07:23:33 +0200

    add create key button

--- lib/Presentation/UI/LockDetails/Widgets/GalleryCardWidget.dart
+++ lib/Presentation/UI/LockDetails/Widgets/GalleryCardWidget.dart
@@ -11,141 +11,7 @@ class GalleryCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (images.length == 1) {
-      return Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SizedBox(
-          height: 220,
-          child: Row(
-            children: [
-              Expanded(
-                  child: ImageWidget(
-                onImageClick: onImageClick,
-                image: images[0],
-                tag: "0",
-              )),
-            ],
-          ),
-        ),
-      );
-    }
-    else if (images.length == 2) {
-      return Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SizedBox(
-          height: 220,
-          child: Row(
-            children: [
-              Expanded(
-                  flex: 2,
-                  child: ImageWidget(
-                    onImageClick: onImageClick,
-                    image: images[0],
-                    tag: "0",
-                  )),
-              const SizedBox(width: 10),
-              Expanded(
-                  child: ImageWidget(
-                onImageClick: onImageClick,
-                image: images[1],
-                tag: "1",
-              )),
-            ],
-          ),
-        ),
-      );
-    }
-    else if (images.length == 3) {
-      return Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SizedBox(
-          height: 220,
-          child: Row(
-            children: [
-              Expanded(
-                  flex: 2,
-                  child: ImageWidget(
-                      onImageClick: onImageClick, image: images[0], tag: "0")),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Expanded(
-                              child: ImageWidget(
-                                  onImageClick: onImageClick,
-                                  image: images[1],
-                                  tag: "1")),
-                          const SizedBox(height: 10),
-                          Expanded(
-                              child: ImageWidget(
-                                  onImageClick: onImageClick,
-                                  image: images[2],
-                                  tag: "2")),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      );
-    }
-    else if (images.length == 4) {
-      return Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SizedBox(
-          height: 220,
-          child: Row(
-            children: [
-              Expanded(
-                  child: ImageWidget(
-                onImageClick: onImageClick,
-                image: images[0],
-                tag: "0",
-              )),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Column(
-                  children: [
-                    Expanded(
-                        child: ImageWidget(
-                      onImageClick: onImageClick,
-                      image: images[1],
-                      tag: "1",
-                    )),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Expanded(
-                              child: ImageWidget(
-                                  onImageClick: onImageClick,
-                                  image: images[2],
-                                  tag: "2")),
-                          const SizedBox(width: 10),
-                          Expanded(
-                              child: ImageWidget(
-                                  onImageClick: onImageClick,
-                                  image: images[3],
-                                  tag: "3")),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      );
-    }
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: SizedBox(
+      return SizedBox(
         height: 220,
         child: Row(
           children: [
@@ -155,6 +21,42 @@ class GalleryCardWidget extends StatelessWidget {
               image: images[0],
               tag: "0",
             )),
+          ],
+        ),
+      );
+    }
+    else if (images.length == 2) {
+      return SizedBox(
+        height: 220,
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: ImageWidget(
+                  onImageClick: onImageClick,
+                  image: images[0],
+                  tag: "0",
+                )),
+            const SizedBox(width: 10),
+            Expanded(
+                child: ImageWidget(
+              onImageClick: onImageClick,
+              image: images[1],
+              tag: "1",
+            )),
+          ],
+        ),
+      );
+    }
+    else if (images.length == 3) {
+      return SizedBox(
+        height: 220,
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: ImageWidget(
+                    onImageClick: onImageClick, image: images[0], tag: "0")),
             const SizedBox(width: 10),
             Expanded(
               child: Row(
@@ -176,44 +78,49 @@ class GalleryCardWidget extends StatelessWidget {
                       ],
                     ),
                   ),
-                  const SizedBox(width: 10),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    else if (images.length == 4) {
+      return SizedBox(
+        height: 220,
+        child: Row(
+          children: [
+            Expanded(
+                child: ImageWidget(
+              onImageClick: onImageClick,
+              image: images[0],
+              tag: "0",
+            )),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                children: [
                   Expanded(
-                    child: Column(
+                      child: ImageWidget(
+                    onImageClick: onImageClick,
+                    image: images[1],
+                    tag: "1",
+                  )),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    child: Row(
                       children: [
+                        Expanded(
+                            child: ImageWidget(
+                                onImageClick: onImageClick,
+                                image: images[2],
+                                tag: "2")),
+                        const SizedBox(width: 10),
                         Expanded(
                             child: ImageWidget(
                                 onImageClick: onImageClick,
                                 image: images[3],
                                 tag: "3")),
-                        const SizedBox(height: 10),
-                        Expanded(
-                            child: Stack(
-                              children: [
-                                ImageWidget(
-                                    onImageClick: onImageClick,
-                                    image: images[4],
-                                    tag: "4"),
-                                Positioned.fill(
-                                    child: InkWell(
-                                      overlayColor: MaterialStateProperty.all(Colors.transparent),
-                                      onTap: () => onMoreImagesPress(),
-                                      child: Container(
-                                        padding: const EdgeInsets.all(15),
-                                        decoration: BoxDecoration(
-                                          color: Theme.of(context).primaryColor.withOpacity(0.7),
-                                          borderRadius: BorderRadius.circular(10)
-                                        ),
-                                        child: FittedBox(
-                                          child: Text(
-                                            "+${images.length-4}",
-                                            style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).scaffoldBackgroundColor),
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                )
-                              ],
-                            )),
                       ],
                     ),
                   ),
@@ -222,6 +129,84 @@ class GalleryCardWidget extends StatelessWidget {
             )
           ],
         ),
+      );
+    }
+    return SizedBox(
+      height: 220,
+      child: Row(
+        children: [
+          Expanded(
+              child: ImageWidget(
+            onImageClick: onImageClick,
+            image: images[0],
+            tag: "0",
+          )),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: ImageWidget(
+                              onImageClick: onImageClick,
+                              image: images[1],
+                              tag: "1")),
+                      const SizedBox(height: 10),
+                      Expanded(
+                          child: ImageWidget(
+                              onImageClick: onImageClick,
+                              image: images[2],
+                              tag: "2")),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: ImageWidget(
+                              onImageClick: onImageClick,
+                              image: images[3],
+                              tag: "3")),
+                      const SizedBox(height: 10),
+                      Expanded(
+                          child: Stack(
+                            children: [
+                              ImageWidget(
+                                  onImageClick: onImageClick,
+                                  image: images[4],
+                                  tag: "4"),
+                              Positioned.fill(
+                                  child: InkWell(
+                                    overlayColor: MaterialStateProperty.all(Colors.transparent),
+                                    onTap: () => onMoreImagesPress(),
+                                    child: Container(
+                                      padding: const EdgeInsets.all(15),
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context).primaryColor.withOpacity(0.7),
+                                        borderRadius: BorderRadius.circular(10)
+                                      ),
+                                      child: FittedBox(
+                                        child: Text(
+                                          "+${images.length-4}",
+                                          style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).scaffoldBackgroundColor),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                              )
+                            ],
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
